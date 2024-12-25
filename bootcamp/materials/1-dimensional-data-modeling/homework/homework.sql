faccbbd5d4e3b9a9686a55a094fe2500038c9294
Berkay Günes <berkayguenes@Berkays-Mini.fritz.box>
2024-12-25 09:00:50 +0100

    finish exercise 5 for the homework of part 1

--- bootcamp/materials/1-dimensional-data-modeling/homework/homework.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework.sql
@@ -172,7 +172,12 @@ this_year_data as (
 ),
 
 unchanged_records as (
-	select *
+	select
+		ty.actor,
+		ly.quality_class,
+		ly.is_active,
+		ly.start_year,
+		ty.year as end_year
 	from this_year_data ty
 	join last_year_scd ly
 		on ty.actor=ly.actor
@@ -196,8 +201,38 @@ changed_records as (
 	where 1=1
 		and (ty.quality_class <> ly.quality_class OR ty.is_active <> ly.is_active)
 		and ly.actor is not null
+),
+
+unnested_changed_records as (
+	select
+		actor,
+		(records::scd_type_actors).*
+	from changed_records
+),
+
+new_records as (
+	select
+		ty.actor,
+        ty.quality_class,
+        ty.is_active,
+        ty.year as start_year,
+        ty.year as end_year
+	from this_year_data ty
+	join last_year_scd ly
+		on ty.actor=ly.actor
+	where ly.actor is null
 )
 
-select actor, (records::scd_type_actors).* from changed_records
+select * from historical_year_scd
 
+union all
 
+select * from unchanged_records
+
+union all
+
+select * from unnested_changed_records
+
+union all
+
+select * from new_records
